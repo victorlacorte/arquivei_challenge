fc2e610dfbd1051f5d393e5708246302ea011bcc
victorlacorte <victorgabriel89@gmail.com>
2019-11-03 15:52:55 -0300

    We have a font related bug

--- src/commons/styles/theme.js
+++ src/commons/styles/theme.js
@@ -1,15 +1,99 @@
-// import 'assets/fonts/roboto.css';
+const Roboto = 'Roboto, sans-serif';
+const Rubik = 'Rubik, sans-serif';
 
+// https://material.io/resources/color/#!/?view.left=0&view.right=1
 const theme = {
   palette: {
     primary: {
-      main: 'pink',
+      light: '#4076fa30',
+      main: '#4076fa',
+      dark: '#004bc6',
+    },
+    success: {
+      light: '#00d23330',
+      main: '#00d233',
+      dark: '#009f00',
+    },
+    error: {
+      light: '#f1502530',
+      main: '#f15025',
+      dark: '#b71300',
+    },
+    alert: {
+      light: '#e8c54730',
+      main: '#e8c547',
+      dark: '#b2950b',
+    },
+    dark: {
+      light: '#00163430',
+      main: '#001634',
+      dark: '#00000f',
+    },
+    extraDark: {
+      light: '#09122330',
+      main: '#091223',
+      dark: '#0000002',
+    },
+    grey: {
+      100: {
+        light: '#f5f5f530',
+        main: '#f5f5f5',
+        dark: '#c2c2c2',
+      },
+      200: {
+        light: '#eeeeee30',
+        main: '#eeeeee',
+        dark: '#bcbcbc',
+      },
+      300: {
+        light: '#e0e0e030',
+        main: '#e0e0e0',
+        dark: '#aeaeae',
+      },
+      400: {
+        light: '#bdbdbd30',
+        main: '#bdbdbd',
+        dark: '#8d8d8d',
+      },
     },
   },
   typography: {
-    font: {
-      Roboto: 'Roboto, sans-serif',
-      Rubik: 'Rubik, sans-serif',
+    h1: {
+      fontFamily: Rubik,
+      fontWeight: 900,
+      fontSize: 42,
+      lineHeight: 1,
+    },
+    h2: {
+      fontFamily: Rubik,
+      fontWeight: 700,
+      fontSize: 22,
+      lineHeight: 1,
+    },
+    body1: {
+      fontFamily: Rubik,
+      fontWeight: 400,
+      fontSize: 18,
+      lineHeight: 27,
+    },
+    body2: {
+      fontFamily: Roboto,
+      fontWeight: 400,
+      fontSize: 18,
+      lineHeight: 27,
+    },
+    button: {
+      fontFamily: Roboto,
+      fontWeight: 500,
+      fontSize: 14,
+      lineHeight: 1.75,
+      textTransform: 'uppercase',
+    },
+    link: {
+      fontFamily: Roboto,
+      fontWeight: 400,
+      fontSize: 16,
+      lineHeight: 1,
     },
   },
 };
