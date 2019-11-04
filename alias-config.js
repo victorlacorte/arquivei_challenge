e6b40f221af696e3fb04aef06aaae7563f9c0de4
victorlacorte <victorgabriel89@gmail.com>
2019-11-04 12:51:20 -0300

    Included component alias

--- alias-config.js
+++ alias-config.js
@@ -3,4 +3,5 @@ const path = require('path');
 module.exports = {
   assets: path.join(__dirname, 'src/assets'),
   commons: path.join(__dirname, 'src/commons'),
+  components: path.join(__dirname, 'src/components'),
 };
