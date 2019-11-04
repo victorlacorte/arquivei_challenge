1a5fca3bc3e4f16f3809fc6e3b1d7f1516336b2e
victorlacorte <victorgabriel89@gmail.com>
2019-11-04 18:07:36 -0300

    Purchase summary seems good for now; still needs to be tested

---
+++ pages/Consults/components/PurchaseSummary/PurchaseSummary.jsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { toBRL } from 'commons/utils/formatting';
+
+import {
+  DiscountedTotal,
+  Hr,
+  OriginalTotal,
+  Receipt,
+  Table,
+  Title,
+  Total,
+} from './styles';
+
+const PurchaseSummary = (props) => {
+  const {
+    totalKeys,
+    keyUnitPrice,
+    originalTotal,
+    discountedTotal,
+    percentageDiscount,
+  } = props;
+
+  return (
+    <Receipt>
+      <Title>
+        Detalhes da compra
+      </Title>
+
+      <Table>
+        <thead>
+          <tr>
+            <th># Chaves</th>
+            <th>Valor unitário</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>{totalKeys}</td>
+            <td>
+              {toBRL(keyUnitPrice)}
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+
+      <Hr />
+
+      <Title>
+        Total
+      </Title>
+
+      {discountedTotal
+        ? (
+          <fragment>
+            <OriginalTotal>
+              De:
+              <span>
+                {toBRL(originalTotal)}
+              </span>
+            </OriginalTotal>
+
+            <DiscountedTotal>
+              <span>
+                {toBRL(discountedTotal)}
+              </span>
+
+              <span>
+                (
+                {Number(percentageDiscount).toFixed(0)}
+                % off)
+              </span>
+            </DiscountedTotal>
+          </fragment>
+        )
+        : (
+          <Total>
+            {toBRL(originalTotal)}
+          </Total>
+        )}
+    </Receipt>
+  );
+};
+
+PurchaseSummary.defaultProps = {
+  discountedTotal: null,
+
+  percentageDiscount: null,
+};
+
+PurchaseSummary.propTypes = {
+  totalKeys: PropTypes.number.isRequired,
+
+  keyUnitPrice: PropTypes.number.isRequired,
+
+  originalTotal: PropTypes.number.isRequired,
+
+  discountedTotal: PropTypes.number,
+
+  percentageDiscount: PropTypes.number,
+};
+
+export default PurchaseSummary;
---
+++ pages/Consults/components/PurchaseSummary/styles.js
@@ -0,0 +1,83 @@
+import styled, { css } from 'styled-components';
+
+const Receipt = styled.div`
+  padding: 24px;
+  background-color: ${({ theme }) => theme.palette.grey100.main};
+  border-style: solid;
+  border-width: 0 0 10px 0;
+  border-image-source: url("/images/receipt-border.png");
+  border-image-slice: 30;
+  border-image-width: 1;
+  border-image-outset: 0;
+  border-image-repeat: stretch;
+`;
+
+const Title = styled.h2`
+  text-align: center;
+  margin-bottom: 24px;
+
+  ${({ theme }) => css`
+      ${theme.typography.h2};
+  `}
+`;
+
+const Hr = styled.hr`
+  border-style: dashed;
+  border-color: ${({ theme }) => theme.palette.grey100.dark};
+  margin-bottom: 24px;
+`;
+
+// TODO set Table's max-width
+const Table = styled.table`
+  width: 100%;
+  table-layout: fixed;
+  border-collapse: collapse;
+  margin-bottom: 24px;
+
+  & th {
+    color: ${({ theme }) => theme.palette.grey400.main};
+    font-family: ${({ theme }) => theme.typography.fontFamily.Rubik};
+  }
+
+  & td {
+    font-family: ${({ theme }) => theme.typography.fontFamily.Roboto};
+    text-align: center;
+  }
+`;
+
+const OriginalTotal = styled.div`
+  color: ${({ theme }) => theme.palette.grey400.main};
+  font-family: ${({ theme }) => theme.typography.fontFamily.Roboto};
+
+  & span {
+    margin-left: 8px;
+    text-decoration: line-through;
+  }
+`;
+
+const DiscountedTotal = styled.div`
+  font-family: ${({ theme }) => theme.typography.fontFamily.Roboto};
+  font-size: 20px;
+  font-weight: 900;
+  color: ${({ theme }) => theme.palette.success.main};
+
+  & span:not(:first-child) {
+    margin-left: 8px;
+  }
+`;
+
+const Total = styled.div`
+  font-family: ${({ theme }) => theme.typography.fontFamily.Roboto};
+  font-size: 20px;
+  font-weight: 900;
+`;
+
+export {
+  Receipt,
+  Title,
+  Hr,
+  Table,
+  OriginalTotal,
+  DiscountedTotal,
+  Total,
+};
