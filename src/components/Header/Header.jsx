2ab706afc3911921dd19f80e35433cff6d6ea92c
victorlacorte <victorgabriel89@gmail.com>
2019-11-04 12:42:17 -0300

    Header done for now, but needs testing

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,34 @@
+import React, { memo } from 'react';
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  position: relative;
+  background-color: ${({ theme }) => theme.palette.primary.main};
+  height: 60px;
+  padding: 16px;
+
+  ${({ theme }) => theme.breakpoints.down('sm')`
+    height: 40px;
+    padding: 8px;
+  `}
+`;
+
+const StyledImg = styled.img`
+  position: absolute;
+  top: 50%;
+  transform: translate(0, -50%);
+  width: 30%;
+  height: auto;
+
+  ${({ theme }) => theme.breakpoints.down('sm')`
+    width: 50%;
+  `}
+`;
+
+const Header = () => (
+  <StyledHeader>
+    <StyledImg src="/images/arquivei-lite.gif" alt="Arquivei Lite logo" />
+  </StyledHeader>
+);
+
+export default memo(Header);
