e748e8e69dfc4ebd887fa592fcadc44fda2f5188
victorlacorte <victorgabriel89@gmail.com>
2019-11-07 07:46:53 -0300

    Some of these configs might be unnecessary; needs further testing

--- alias-config.js
+++ alias-config.js
@@ -3,6 +3,8 @@ const path = require('path');
 module.exports = {
   commons: path.join(__dirname, 'src/commons'),
   components: path.join(__dirname, 'src/components'),
+  config: path.join(__dirname, 'config'),
   lib: path.join(__dirname, 'lib'),
   pages: path.join(__dirname, 'pages'),
+  resources: path.join(__dirname, 'src/resources'),
 };
