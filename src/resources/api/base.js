20fb5554653ce7c2b11e1b85e4c347df686b6682
Victor <victorlacorte@live.com>
2019-11-07 17:06:18 -0300

    Unnecessary comment

--- src/resources/api/base.js
+++ src/resources/api/base.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 import config from 'config';
 
-// No CORS since we're only mocking API calls
 const base = axios.create({
   headers: {
     Accept: 'application/json',
