3462f441fc82ffa6e1f2f672fae5ada743fdebe5
Victor <victorlacorte@live.com>
2019-11-07 11:40:30 -0300

    Removed unnecessary validations

--- src/commons/utils/validations/__tests__/cnpj.test.js
+++ src/commons/utils/validations/__tests__/cnpj.test.js
@@ -1,52 +1,15 @@
 import cnpj from '../cnpj';
 
 describe('CNPJ', () => {
-  it('blacklists common numbers', () => {
-    expect(cnpj.isValidCnpj('00000000000000')).toBe(false);
-    expect(cnpj.isValidCnpj('11111111111111')).toBe(false);
-    expect(cnpj.isValidCnpj('22222222222222')).toBe(false);
-    expect(cnpj.isValidCnpj('33333333333333')).toBe(false);
-    expect(cnpj.isValidCnpj('44444444444444')).toBe(false);
-    expect(cnpj.isValidCnpj('55555555555555')).toBe(false);
-    expect(cnpj.isValidCnpj('66666666666666')).toBe(false);
-    expect(cnpj.isValidCnpj('77777777777777')).toBe(false);
-    expect(cnpj.isValidCnpj('88888888888888')).toBe(false);
-    expect(cnpj.isValidCnpj('99999999999999')).toBe(false);
-  });
-
-  it('rejects falsy values', () => {
-    expect(cnpj.isValidCnpj('')).toBe(false);
-    expect(cnpj.isValidCnpj(null)).toBe(false);
-    expect(cnpj.isValidCnpj(undefined)).toBe(false);
-  });
-
-  it('validates formatted strings', () => {
-    expect(cnpj.isValidCnpj('54.550.752/0001-55')).toBe(true);
-  });
-
-  it('validates unformatted strings', () => {
-    expect(cnpj.isValidCnpj('54550752000155')).toBe(true);
-  });
-
-  it('validates messed strings', () => {
-    expect(cnpj.isValidCnpj('54550[752#0001..$55')).toBe(true);
-  });
-
-  it('strictly validates strings', () => {
-    expect(cnpj.isValidCnpj('54550[752#0001..$55', true)).toBe(false);
-    expect(cnpj.isValidCnpj('54.550.752/0001-55', true)).toBe(true);
-    expect(cnpj.isValidCnpj('54550752000155', true)).toBe(true);
-  });
-
-  it('returns stripped number', () => {
-    const number = cnpj.strip('54550[752#0001..$55');
-
-    expect(number).toBe('54550752000155');
-  });
-
-  it('returns formatted number', () => {
-    const number = cnpj.format('54550752000155');
-
-    expect(number).toBe('54.550.752/0001-55');
+  it('normalizes a CNPJ', () => {
+    expect(cnpj.normalizeCNPJ('12')).toEqual('12');
+    expect(cnpj.normalizeCNPJ('123')).toEqual('12.3');
+    expect(cnpj.normalizeCNPJ('12345')).toEqual('12.345');
+    expect(cnpj.normalizeCNPJ('123456')).toEqual('12.345.6');
+    expect(cnpj.normalizeCNPJ('12345678')).toEqual('12.345.678');
+    expect(cnpj.normalizeCNPJ('123456789')).toEqual('12.345.678/9');
+    expect(cnpj.normalizeCNPJ('123456789012')).toEqual('12.345.678/9012');
+    expect(cnpj.normalizeCNPJ('12345678901234')).toEqual('12.345.678/9012-34');
+    expect(cnpj.normalizeCNPJ('123456789012345')).toEqual('12.345.678/9012-34');
   });
 });
--- src/commons/utils/validations/cnpj.js
+++ src/commons/utils/validations/cnpj.js
@@ -1,67 +1,5 @@
-// Blacklist common values.
-const BLACKLIST = [
-  '0'.repeat(14),
-  '1'.repeat(14),
-  '2'.repeat(14),
-  '3'.repeat(14),
-  '4'.repeat(14),
-  '5'.repeat(14),
-  '6'.repeat(14),
-  '7'.repeat(14),
-  '8'.repeat(14),
-  '9'.repeat(14),
-];
-
-const STRICT_STRIP_REGEX = /[-/.]/g;
-const LOOSE_STRIP_REGEX = /[^\d]/g;
-
 /**
-* Compute the CNPJ's Verifier Digit (or "Dígito Verificador (DV)" in portuguese).
-*
-* More info on [wikipedia (pt-br)](https://pt.wikipedia.org/wiki/D%C3%ADgito_verificador)
-*
-* @param {string} numbers the CNPJ string with only numbers.
-* @returns {number} the verifier digit.
-*/
-function verifierDigit(numbers) {
-  let index = 2;
-  const reverse = numbers.split('').reduce((buffer, number) => [parseInt(number, 10)].concat(buffer), []);
-
-  const sum = reverse.reduce((buffer, number) => {
-    const newBuffer = buffer + number * index;
-
-    index = (index === 9 ? 2 : index + 1);
-
-    return newBuffer;
-  }, 0);
-
-  const mod = sum % 11;
-
-  return (mod < 2 ? 0 : 11 - mod);
-}
-
-/**
-* Remove some characters from the input.
-*
-* Example:
-* ```
-* strip('54550[752#0001..$55'); // Result: '54550752000155'
-* strip('54550[752#0001..$55', true); // Result: '54550[752#0001..$55' - Atention!
-* ```
-*
-* @param {string} cnpj the CNPJ text.
-* @param {boolean} [isStrict] if `true`, it will remove only `.` and `-` characters.
-* Otherwise, it will remove all non-digit (`[^\d]`) characters. Optional.
-* @returns {string} the stripped CNPJ.
-*/
-function strip(cnpj, isStrict) {
-  const regex = isStrict ? STRICT_STRIP_REGEX : LOOSE_STRIP_REGEX;
-
-  return (cnpj || '').toString().replace(regex, '');
-}
-
-/**
-* Transform the input into a pretty CNPJ format.
+* Transform the input into a pretty CNPJ format and limit its length to 14 (numeric) characters
 *
 * Example:
 * ```
@@ -69,42 +7,35 @@ function strip(cnpj, isStrict) {
 * // Result: '54.550.752/0001-55'
 * ```
 *
-* @param {string} cnpj the CNPJ.
+* @param {string} value the CNPJ value.
 * @returns {string} the formatted CNPJ.
 */
-function format(cnpj) {
-  return strip(cnpj).replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/, '$1.$2.$3/$4-$5');
-}
+function normalizeCNPJ(value) {
+  if (!value) {
+    return value;
+  }
 
-/**
-* Validate the CNPJ.
-*
-* @param {string} cnpj the CNPJ number.
-* @param {boolean} [isStrict] if `true`, it will accept only `digits`, `.` and `-` characters. Optional.
-* @returns {boolean} `true` if CNPJ is valid. Otherwise, `false`.
-*/
-function isValidCnpj(cnpj, isStrict) { // eslint-disable-line complexity
-  const stripped = strip(cnpj, isStrict);
+  const onlyNums = value.toString().replace(/[^\d]/g, '');
 
-  // CNPJ must be defined
-  if (!stripped) { return false; }
+  if (onlyNums.length < 3) {
+    return onlyNums;
+  }
 
-  // CNPJ must have 14 chars
-  if (stripped.length !== 14) { return false; }
+  if (onlyNums.length < 6) {
+    return `${onlyNums.slice(0, 2)}.${onlyNums.slice(2)}`;
+  }
 
-  // CNPJ can't be blacklisted
-  if (BLACKLIST.includes(stripped)) { return false; }
+  if (onlyNums.length < 9) {
+    return `${onlyNums.slice(0, 2)}.${onlyNums.slice(2, 5)}.${onlyNums.slice(5)}`;
+  }
 
-  let numbers = stripped.substr(0, 12);
-  numbers += verifierDigit(numbers);
-  numbers += verifierDigit(numbers);
+  if (onlyNums.length < 13) {
+    return `${onlyNums.slice(0, 2)}.${onlyNums.slice(2, 5)}.${onlyNums.slice(5, 8)}/${onlyNums.slice(8)}`;
+  }
 
-  return numbers.substr(-2) === stripped.substr(-2);
+  return `${onlyNums.slice(0, 2)}.${onlyNums.slice(2, 5)}.${onlyNums.slice(5, 8)}/${onlyNums.slice(8, 12)}-${onlyNums.slice(12, 14)}`;
 }
 
 export default {
-  format,
-  isValidCnpj,
-  strip,
-  verifierDigit,
+  normalizeCNPJ,
 };
