ebd3173d060ba23c819bbab00f02f3a43f5795d4
Victor <victorlacorte@live.com>
2019-11-07 17:07:46 -0300

    More form validations

--- src/commons/utils/validations/formValidations.js
+++ src/commons/utils/validations/formValidations.js
@@ -1,3 +1,5 @@
+// TODO sanitize these functions and remove unnecessary ones
+
 function required(value) {
   if (value) {
     return undefined;
@@ -9,6 +11,19 @@ function required(value) {
 function minLength(min) {
   return function validate(value) {
     if (value && value.length < min) {
+      return `São necessários no mínimo ${min} caracteres`;
+    }
+
+    return undefined;
+  };
+}
+
+// Meant to be applied to values with "mask" characters
+function minNumLength(min) {
+  return function validate(value) {
+    const onlyNums = value.toString().replace(/[^\d]/g, '');
+
+    if (onlyNums && onlyNums.toString().length < min) {
       return `São necessários no mínimo ${min} caracteres`;
     }
 
@@ -58,6 +73,7 @@ function email(value) {
 export default {
   required,
   minLength,
+  minNumLength,
   maxLength,
   minValue,
   maxValue,
