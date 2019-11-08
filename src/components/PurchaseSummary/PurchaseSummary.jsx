e3fb4fceb05257e82288444a35e1dba168ad4a47
Victor <victorlacorte@live.com>
2019-11-08 11:35:04 -0300

    Standardized the function Component

--- src/components/PurchaseSummary/PurchaseSummary.jsx
+++ src/components/PurchaseSummary/PurchaseSummary.jsx
@@ -13,7 +13,7 @@ import {
   Total,
 } from './styles';
 
-const PurchaseSummary = (props) => {
+function PurchaseSummary(props) {
   const {
     consultKeys,
     keyUnitPrice,
@@ -82,7 +82,7 @@ const PurchaseSummary = (props) => {
         )}
     </Receipt>
   );
-};
+}
 
 PurchaseSummary.defaultProps = {
   consultKeys: 0,
