82a4e1428a235b6040f0e9dae4054fbf83f4a082
Victor <victorlacorte@live.com>
2019-11-08 11:35:33 -0300

    PurchaseSummary initial tests

---
+++ src/components/PurchaseSummary/__tests__/PurchaseSummary.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import PurchaseSummary from 'components/PurchaseSummary';
+import { shallowWithTheme } from 'commons/utils/testUtils';
+
+describe('<PurchaseSummary />', () => {
+  it('creates a PurchaseSummary component', () => {
+    // eslint-disable-next-line react/jsx-filename-extension
+    const wrapper = shallowWithTheme(<PurchaseSummary />);
+
+    // Default values have no discount
+    expect(wrapper.find('styles__DiscountedTotal').children()).toHaveLength(0);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('creates a PurchaseSummary component with total and discountedTotal props', () => {
+    const wrapper = shallowWithTheme(<PurchaseSummary total={10} discountedTotal={8} />);
+
+    expect(wrapper.find('styles__OriginalTotal').children()).toHaveLength(2);
+    expect(wrapper.find('styles__DiscountedTotal').children()).toHaveLength(2);
+  });
+});
